d3101e979ada6258a5efc4d3678d7e7c9a407a15
Nirajan <nirajanb89@gmail.com>
2017-02-10 16:25:45 +0545

    Initial Setup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var elixir = require('laravel-elixir');
+
+elixir(function (mix) {
+    mix.sass('./assets/css/sass/app.scss', './public/css')
+        .styles([
+            './assets/css/vendor/normalize.css',
+            './assets/css/vendor/bootstrap.min.css'
+        ], './public/css/vendor.css');
+
+
+    mix.scripts([
+            './assets/js/vendor/jquery-1.8.0.min.js',
+            './assets/js/vendor/modernizr-2.8.3.min.js',
+            './assets/js/vendor/bootstrap.min.js'
+        ], './public/js/vendor.js');
+});
